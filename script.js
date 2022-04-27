8a6f19e20741a3743e651b92055be3636854b8a1
Андрей <papeslaver@gmail.com>
2022-04-27 22:16:08 +0300

    initial

---
+++ script.js
@@ -0,0 +1,7 @@
+const productName = 'Кресло';
+const productAmount = 5;
+const productCategory = 'Мебель';
+const productPrice = 14000;
+
+console.log(productName);
+console.log('Общая сумма товара: ' + productAmount * productPrice + '₽');
