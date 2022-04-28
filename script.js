5eeed0a508b9f7f48097510008b5a56b1e00748d
Андрей <awesomeoverlord@mail.ru>
2022-04-28 04:30:50 +0300

    Добавлен новый товар

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const productName = 'Кресло';
-const productAmount = 5;
-const productCategory = 'Мебель';
-const productPrice = 14000;
+'use strict';
 
-console.log(productName);
-console.log('Общая сумма товара: ' + productAmount * productPrice + '₽');
+{
+  const productName = 'Кресло';
+  const productAmount = 5;
+  const productCategory = 'Мебель';
+  const productPrice = 14000;
+
+  console.log(productName);
+  console.log('Общая сумма товара: ' + productAmount * productPrice + '₽');
+}
+
+{
+  const productName = 'Пылесос';
+  const productAmount = 14;
+  const productCategory = 'Бытовая техника';
+  const productPrice = 8000;
+}
