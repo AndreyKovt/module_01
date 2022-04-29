f620604c80493e61544cfe43dc71ddcc0cf68e8e
Андрей <awesomeoverlord@mail.ru>
2022-04-29 10:13:31 +0300

    задание к уроку 3

---
+++ lesson_03/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+{
+  const productName = 'Кресло';
+  const productAmount = 5;
+  const productCategory = 'Мебель';
+  const productPrice = 14000;
+
+  console.log(productName);
+  console.log('Общая сумма товара: ' + productAmount * productPrice + '₽');
+}
+
+{
+  const productName = 'Пылесос';
+  const productAmount = 14;
+  const productCategory = 'Бытовая техника';
+  const productPrice = 8000;
+}
+
+{
+  const productNameFromUser = prompt('Введите наименование товара');
+  const productAmountFromUser = prompt('Введите количество товара');
+  const productCategoryFromUser = prompt('Введите категорию товара');
+  const productPriceFromUser = prompt('Введите стоимость товара');
+
+  console.log(
+    `На складе ${productAmountFromUser} единиц товара "${productNameFromUser}" категории "${productCategoryFromUser}" на сумму ${
+      productPriceFromUser * productAmountFromUser
+    } деревянных.`
+  );
+}
